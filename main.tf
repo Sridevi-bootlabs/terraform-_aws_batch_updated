c4c14fc4d0182137c772789ac09e4b1d2c8cdc31
Sridevi-bootlabs <105430846+Sridevi-bootlabs@users.noreply.github.com>
2022-10-19 17:26:12 +0530

    adding the policy

--- main.tf
+++ main.tf
@@ -213,3 +213,27 @@ resource "aws_iam_role" "example" {
 }
 EOF
 }
+
+resource "aws_iam_role_policy" "example" {
+  name = "example"
+  role = aws_iam_role.example.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
