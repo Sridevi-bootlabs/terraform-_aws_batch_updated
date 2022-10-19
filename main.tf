b800495f58e9354eab6cdb4619357555853fb4b7
Sridevi-bootlabs <105430846+Sridevi-bootlabs@users.noreply.github.com>
2022-10-19 17:10:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -182,3 +182,34 @@ resource "aws_batch_scheduling_policy" "example" {
     "Name" = "Example Batch Scheduling Policy"
   }
 }
+
+resource "aws_flow_log" "example" {
+  iam_role_arn    = aws_iam_role.example.arn
+  log_destination = aws_cloudwatch_log_group.example.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  name = "example"
+}
+
+resource "aws_iam_role" "example" {
+  name = "example"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
