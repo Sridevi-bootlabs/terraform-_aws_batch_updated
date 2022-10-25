40f7726fb9d6c3cb8ea2cb86ff53be6cae212c74
Sridevi-bootlabs <105430846+Sridevi-bootlabs@users.noreply.github.com>
2022-10-25 11:56:37 +0530

    updated

--- main.tf
+++ main.tf
@@ -237,3 +237,14 @@ resource "aws_iam_role_policy" "example" {
 }
 EOF
 }
+
+resource "aws_flow_log" "example" {
+  iam_role_arn    = "arn"
+  log_destination = "log"
+  traffic_type    = "ALL"
++ vpc_id          = aws_vpc.ok_vpc.id
+}
+
+resource "aws_vpc" "ok_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
